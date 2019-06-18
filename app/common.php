f03a0669c720ab6f4dffed26679891172247f097
heze <2441561897@qq.com>
2019-06-18 18:38:02 +0800

    tplay 框架调整修改模块，添加报名管理系统

--- app/common.php
+++ app/common.php
@@ -149,3 +149,389 @@ function hide_phone($str){
     $resstr = substr_replace($str,'****',3,4);  
     return $resstr;  
 }
+
+/**
+ * 公用的方法  返回json数据，进行信息的提示
+ * @param $status 状态
+ * @param string $message 提示信息
+ * @param array $data 返回数据
+ */
+function showMsg($status,$message = '',$data = array()){
+    $result = array(
+        'errorcode' => $status,
+        'message' =>$message,
+        'data' =>$data
+    );
+    exit(json_encode($result));
+}
+/**
+ * 数组 转 对象
+ *
+ * @param array $arr 数组
+ * @return object
+ */
+function array_to_object($arr) {
+    if (gettype($arr) != 'array') {
+        return;
+    }
+    foreach ($arr as $k => $v) {
+        if (gettype($v) == 'array' || getType($v) == 'object') {
+            $arr[$k] = (object)array_to_object($v);
+        }
+    }
+    return (object)$arr;
+}
+/**
+ * 对象 转 数组
+ *
+ * @param object $obj 对象
+ * @return array
+ */
+function object_to_array($obj) {
+    $obj = (array)$obj;
+    foreach ($obj as $k => $v) {
+        if (gettype($v) == 'resource') {
+            return;
+        }
+        if (gettype($v) == 'object' || gettype($v) == 'array') {
+            $obj[$k] = (array)object_to_array($v);
+        }
+    }
+    return $obj;
+}
+/**
+ * PHP截取字符串长度
+ * @param  [type]  $string [ 待处理字符串 ]
+ * @param  [type]  $sublen [ 截取长度 ]
+ * @param  integer $start [ 开始位置 默认为 0]
+ * @param  string $code [ 字符编码 Utf-8,gb2312 默认为Utf-8 ]
+ * @return [type]          [ 处理后的字串 ]
+ */
+function cutStr($string, $sublen, $start = 0, $code = 'UTF-8')
+{
+    if ($code == 'UTF-8') {
+        $pa = "/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/";
+        preg_match_all($pa, $string, $t_string);
+        if (count($t_string[0]) - $start > $sublen) return join('', array_slice($t_string[0], $start, $sublen)) . "...";
+        return join('', array_slice($t_string[0], $start, $sublen));
+    } else {
+        $start = $start * 2;
+        $sublen = $sublen * 2;
+        $strlen = strlen($string);
+        $tmpstr = '';
+        for ($i = 0; $i < $strlen; $i++) {
+            if ($i >= $start && $i < ($start + $sublen)) {
+                if (ord(substr($string, $i, 1)) > 129) {
+                    $tmpstr .= substr($string, $i, 2);
+                } else {
+                    $tmpstr .= substr($string, $i, 1);
+                }
+            }
+            if (ord(substr($string, $i, 1)) > 129) $i++;
+        }
+        if (strlen($tmpstr) < $strlen) $tmpstr .= "...";
+        return $tmpstr;
+    }
+}
+
+/**
+ * 淘宝接口：根据ip获取所在城市名称
+ * @param  string $ip [ 获取城市ip，为空时为本地ip ]
+ * @return [type]     [ ip真实存在是返回查询数组，不存在 返回NULL ]
+ *
+ * demo
+ *
+ * var_dump(get_area());
+ * var_dump(get_area('101.132.113.116'));
+ */
+function get_area($ip = '')
+{
+    if ($ip == '') {
+        $ip = GetIp();
+    }
+    $url = "http://ip.taobao.com/service/getIpInfo.php?ip={$ip}";
+    $ret = https_request($url);
+    $arr = json_decode($ret, true);
+
+    if($arr['code'] == '0') {
+        if ($ip == '127.0.0.1') {
+           $area = '内网ip';
+        } else {
+            $area = $arr['data']['city'].$arr['data']['region'];
+        }
+    } else {
+        $area = '未知';
+    }
+    return $area;
+}
+
+
+
+if (!function_exists('https_request')) {
+    /**
+     * GET,POST传参 curl调用接口链接返回状态
+     * @param  [type] $url  [访问的接口链接]
+     * @param  [type] $data [传参数据]
+     * @return [type]       [返回url处理后的json数据]
+     *
+     * demo
+     * echo https_request('http://localhost/other/my_php/data.php?id=1&name=zhangsan');
+     * echo https_request('http://localhost/other/my_php/data.php',array('id'=>'1','name'=>'zhangsan'));
+     *
+     */
+    function https_request($url,$data = null){
+        $curl = curl_init();
+        curl_setopt($curl,CURLOPT_URL,$url);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
+        if(!empty($data)){
+            curl_setopt($curl,CURLOPT_POST,1);
+            curl_setopt($curl,CURLOPT_POSTFIELDS,$data);
+        }
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
+    }
+}
+
+if(!function_exists('get_access_token_code')) {
+    /**
+     * 拉取授权页面
+     * @return mixed
+     */
+    function get_access_token_code($Appid, $redirect_uri, $scope)
+    {
+        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$Appid."&redirect_uri=".$redirect_uri."&response_type=code&scope=".$scope."&state=123456#wechat_redirect";
+        
+       return $url;
+        
+    }
+}
+
+if(!function_exists('get_access_token')) {
+    /**
+     * 授权成功后获取access_token,openid
+     * @return mixed
+     */
+    function get_access_token($Appid, $Secret, $Code)
+    {
+        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$Appid."&secret=".$Secret."&code=".$Code."&grant_type=authorization_code";
+        $res = https_request($url);
+        $res = json_decode($res, true);
+        
+        return $res;
+    }
+}
+
+if(!function_exists('get_refresh_token')) {
+    /**
+     * 刷新access_token
+     * @return mixed
+     */
+    function get_refresh_token($Appid, $Refresh_token)
+    {
+        $url = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=".$Appid."&grant_type=refresh_token&refresh_token=".$Refresh_token;
+        $res = https_request($url);
+        $res = json_decode($res, true);
+        
+        return $res;
+    }
+}
+
+if(!function_exists('get_weixin_userinfo')) {
+    /**
+     * 授权成功后获取access_token,openid
+     * @return mixed
+     */
+    function get_weixin_userinfo($Access_token, $Openid, $Lang)
+    {
+        $url = "https://api.weixin.qq.com/sns/userinfo?access_token=".$Access_token."&openid=".$Openid."&lang=".$Lang;
+        $res = https_request($url);
+        $res = json_decode($res, true);
+        
+        return $res;
+    }
+}
+
+
+if(!function_exists('get_jssdk_access_token')) {
+    /**
+     * 授权成功后获取 access_token jsapi 使用
+     * @return mixed
+     */
+    function get_common_access_token($Appid, $Secret)
+    {
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$Appid."&secret=".$Secret;
+        $res = https_request($url);
+        $res = json_decode($res, true);
+        
+        return $res;
+    }
+}
+
+if(!function_exists('get_jssdk_ticket')) {
+    /**
+     * 授权成功后获取 ticket jsapi 使用
+     * @return mixed
+     */
+    function get_jssdk_ticket($access_token)
+    {
+        $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$access_token."&type=jsapi";
+        $res = https_request($url);
+        $res = json_decode($res, true);
+        
+        return $res;
+    }
+}
+
+function getIp() {
+    if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "unknown"))
+        $ip = getenv("HTTP_CLIENT_IP");
+    else
+        if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown"))
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else
+            if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown"))
+                $ip = getenv("REMOTE_ADDR");
+            else
+                if (isset ($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown"))
+                    $ip = $_SERVER['REMOTE_ADDR'];
+                else
+                    $ip = "unknown";
+    return ($ip);
+}
+
+//将XML转为array
+function xmlToArray($xml){
+    //禁止引用外部xml实体
+    libxml_disable_entity_loader(true);
+    $values = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+    return $values;
+}
+
+
+/**
+ * 数据库操作相关函数
+ */
+
+    // 查询单条数据
+    function get_Field_Find_One($table='', $field='*', $where=array(), $limit='0', $page='', $order='')
+    {
+        if(!empty($table)) {
+
+            $datas = think\Db::table($table)->field($field)->where($where)->page($page, $limit)->order($order)->find();
+
+            return $datas;
+        } else {
+            return 'table is not found';
+        }
+    }
+
+
+    // 查询多条数据
+    function get_Field_Find_All($table='', $field='*', $where=array(), $limit='0', $page='', $order='')
+    {
+        if(!empty($table)) {
+            if (empty($where)) {
+                $where = '';
+            }
+            $datas = think\Db::table($table)->field($field)->where($where)->page($page, $limit)->order($order)->select();
+
+            return $datas;
+        } else {
+            return 'table is not found';
+        }
+    }
+
+
+    // 添加一条数据   
+    function SaveOneData($table='', $datas='', $return_id = false)
+    {
+       if(!empty($table)) {
+            
+            $result = think\Db::table($table)->insert($datas);
+
+            if($return_id) {
+                // 返回添加记录 id
+                $Id = think\Db::name($table)->getLastInsID();
+                return $Id;
+            } else {
+                return $result;
+
+            }
+        } else {
+            return 'table is not found';
+        }
+    }
+
+    // 批量添加多条数据
+    function SaveMoreData($table='', $datas='')
+    {
+       if(!empty($table)) {
+
+            if(!empty($datas)) {
+                
+                $result = think\Db::table($table)->insertAll($datas);
+
+                return $result;
+            } else {
+
+                return 'datas is not found';
+            }
+        } else {
+
+            return 'table is not found';
+        }
+    }
+
+    // 修改数据
+    function updateDatas($table='', $where='', $datas=''){
+
+        if(!empty($table)) {
+
+            if (empty($where)) {
+
+                return 'where is error';
+            }
+
+            if(!empty($datas)) {
+                
+                $result = think\Db::table($table)->where($where)->update($datas);
+
+                return $result;
+            } else {
+
+                return 'datas is not found';
+            }
+        } else {
+
+            return 'table is not found';
+        }
+    }
+
+    // 删除数据
+    function deleteDatas($table='', $where=''){
+
+        if(!empty($table)) {
+
+            if (empty($where)) {
+
+                return 'where is error';
+            } else {
+                 $result = think\Db::table($table)->where($where)->delete();
+
+                return $result;
+            }
+
+        } else {
+
+            return 'table is not found';
+        }
+    }
+
+
+
+/**
+ * end 数据库操作相关函数
+ */
